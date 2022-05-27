9d20c30acbca488369b98f325d204a4ceb186aaf
wisedev <wisedev.code@gmail.com>
2022-05-27 21:40:13 +0200

    feat: complete observer pattern example

--- Raven.NET.PoC/Program.cs
+++ Raven.NET.PoC/Program.cs
@@ -1,2 +1,25 @@
 ﻿
+using Raven.NET.PoC.Observer;
+using Raven.NET.PoC.Subject;
+
 Console.WriteLine("Hello, World!");
+
+var subject = new Subject();
+var observerA = new Observer();
+subject.Attach(observerA);
+
+var observerB = new Observer();
+subject.Attach(observerB);
+
+subject.State = 5;
+subject.Notify();
+Thread.Sleep(2000);
+
+subject.State = 6;
+subject.Notify();
+Thread.Sleep(5000);
+
+subject.Detach(observerB);
+
+subject.State = 1;
+subject.Notify();
