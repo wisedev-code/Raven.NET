96bd241f0315ed5149461164d63aa1815384378f
wisedev <wisedev.code@gmail.com>
2022-06-01 11:38:52 +0200

    feat: cleanup

---
+++ Demos/Demo.Console/Models/DummySubject.cs
@@ -0,0 +1,6 @@
+namespace Demo.Console.Models;
+
+public class DummySubject
+{
+    
+}
--- Raven.NET.Core/Subjects/RavenSubject.cs
+++ Raven.NET.Core/Subjects/RavenSubject.cs
@@ -1,17 +1,12 @@
+using System;
 using System.ComponentModel;
 using System.Runtime.CompilerServices;
 using Raven.NET.Core.Annotations;
 
 namespace Raven.NET.Core.Subjects
 {
-    public class RavenSubject : INotifyPropertyChanged
+    public class RavenSubject
     {
-        public event PropertyChangedEventHandler PropertyChanged;
 
-        [NotifyPropertyChangedInvocator]
-        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
-        {
-            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
-        }
     }
 }
