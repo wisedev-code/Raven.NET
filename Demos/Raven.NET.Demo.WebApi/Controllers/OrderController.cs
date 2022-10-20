f99aaf6931481360ee5aa1031fe088b9d8564837
piotr <wisedev.pstach@gmail.com>
2022-10-20 20:13:23 +0200

    feat: add new project

---
+++ Demos/Raven.NET.Demo.WebApi/Controllers/OrderController.cs
@@ -0,0 +1,15 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Raven.NET.Demo.WebApi.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class OrderController : ControllerBase
+{
+    private readonly ILogger<OrderController> _logger;
+
+    public OrderController(ILogger<OrderController> logger)
+    {
+        _logger = logger;
+    }
+}
